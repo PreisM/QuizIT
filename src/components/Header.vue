85cc57918fa3019c44af22987b5c81bede2f5a0a
Maciej <maciej.preis@onet.eu>
2020-10-14 09:48:56 +0200

    add i18n

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,16 @@
     <b-nav class="border-bottom">
       <b-nav-item  disabled class="font-weight-bold">IT Quiz</b-nav-item>
       <b-nav-item disabled>{{$t('header.counter')}} {{numCorrect}}/{{ numTotal}} </b-nav-item>
-      <b-nav-item class="text-right fixed-top">
-        <b-button variant="outline-secondary" :to="`/${$i18n.locale}/registration`">{{ $t('header.registration') }}</b-button>
+      <b-nav-item-dropdown>
+        <template v-slot:button-content>
+          {{$t('header.language')}}
+        </template>
+        <b-dropdown-item @click.prevent="setLocale('pl')">PL</b-dropdown-item>
+        <b-dropdown-item @click.prevent="setLocale('en')">EN</b-dropdown-item>
+        <b-dropdown-item @click.prevent="setLocale('esp')">ESP</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item class="position-absolute" style="right:0">
+        <b-button  variant="outline-secondary" :to="`/${$i18n.locale}/registration`">{{ $t('header.registration') }}</b-button>
       </b-nav-item>
     </b-nav>
   </div>
@@ -15,6 +23,18 @@ export default {
   props: [
     'numCorrect',
     'numTotal'
-  ]
+  ],
+  data () {
+    return {
+      langs: ['en', 'pl', 'esp'] }
+  },
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale
+      this.$router.push({
+        params: { lang: locale }
+      })
+    }
+  }
 }
 </script>
